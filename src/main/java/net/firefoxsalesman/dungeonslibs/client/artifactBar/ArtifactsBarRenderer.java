001b285334a281a7246da89821353d9c50c51429
Carson Holscher <firefoxsalesman2@protonmail.com>
2026-08-15 22:10:20 -0400

    Artifact bar renderer actually renders artifacts

--- src/main/java/net/firefoxsalesman/dungeonslibs/client/artifactBar/ArtifactsBarRenderer.java
+++ src/main/java/net/firefoxsalesman/dungeonslibs/client/artifactBar/ArtifactsBarRenderer.java
@@ -3,6 +3,8 @@ package net.firefoxsalesman.dungeonslibs.client.artifactBar;
 import static net.firefoxsalesman.dungeonslibs.utils.ResourceLocationHelper.modLoc;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
@@ -14,10 +16,13 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.GameType;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 import top.theillusivec4.curios.api.type.inventory.IDynamicStackHandler;
 
 /**
@@ -28,46 +33,86 @@ public class ArtifactsBarRenderer {
 	private static final Minecraft minecraft = Minecraft.getInstance();
 	private static final ResourceLocation ARTIFACT_BAR_RESOURCE = modLoc("textures/gui/artifact_bar.png");
 
-	private static boolean noArtifactEquipped(IDynamicStackHandler stacks) {
-		int slots = stacks.getSlots();
-		for (int slot = 0; slot < slots; slot++) {
-			ItemStack artifact = stacks.getStackInSlot(slot);
-			if (!artifact.isEmpty() && artifact.getItem() instanceof ArtifactItem) {
-				return false;
+	private static <U> Optional<U> runWithCuriosHandler(Function<IDynamicStackHandler, U> consoomer) {
+		if (minecraft.getCameraEntity() instanceof Player renderPlayer)
+			return CuriosApi.getCuriosHelper().getCuriosHandler(renderPlayer)
+					.map(iCuriosItemHandler -> {
+						return iCuriosItemHandler.getStacksHandler("artifact")
+								.map(artifactStackHandler -> {
+									return consoomer.apply(artifactStackHandler
+											.getStacks());
+								}).get();
+					});
+		return Optional.empty();
+
+	}
+
+	/**
+	 * Return true if an artifact is equipped
+	 */
+	private static boolean artifactEquipped() {
+		Optional<Boolean> result = runWithCuriosHandler(stacks -> {
+			int slots = stacks.getSlots();
+			for (int slot = 0; slot < slots; slot++) {
+				ItemStack artifact = stacks.getStackInSlot(slot);
+				if (!artifact.isEmpty() && artifact.getItem() instanceof ArtifactItem) {
+					return true;
+				}
 			}
+			return false;
+		});
+		if (result.isPresent()) {
+			return result.get();
 		}
-		return true;
+		return false;
 	}
 
-	public static boolean shouldDisplayBar() {
-		// TODO Call noArtifactEquipped
-		return (minecraft.gameMode != null && minecraft.gameMode.getPlayerMode() != GameType.SPECTATOR);
+	private static boolean shouldDisplayBar() {
+		return artifactEquipped() && minecraft.gameMode != null
+				&& minecraft.gameMode.getPlayerMode() != GameType.SPECTATOR;
 	}
 
-	public static Font getFont() {
+	private static Font getFont() {
 		return minecraft.font;
 	}
 
 	public static void drawHUD(ForgeGui gui, GuiGraphics guiGraphics, float partialTicks, int screenWidth,
 			int screenHeight) {
-		if (!shouldDisplayBar()) {
+		if (!shouldDisplayBar())
 			return;
-		}
 
-		int x = (screenWidth / 2) - 200 + DungeonsLibrariesConfig.SOUL_BAR_HORIZONTAL_OFFSET.get();
-		int y = screenHeight - 21 + DungeonsLibrariesConfig.SOUL_BAR_VERTICAL_OFFSET.get();
+		if (minecraft.getCameraEntity() instanceof Player renderPlayer) {
+			CuriosApi.getCuriosHelper().getCuriosHandler(renderPlayer).ifPresent(iCuriosItemHandler -> {
+				Optional<ICurioStacksHandler> artifactStackHandler = iCuriosItemHandler
+						.getStacksHandler("artifact");
+				if (artifactStackHandler.isPresent()) {
+					IDynamicStackHandler stacks = artifactStackHandler.get().getStacks();
+					int x = (screenWidth / 2) - 200
+							+ DungeonsLibrariesConfig.SOUL_BAR_HORIZONTAL_OFFSET.get();
+					int y = screenHeight - 21
+							+ DungeonsLibrariesConfig.SOUL_BAR_VERTICAL_OFFSET.get();
 
-		RenderSystem.setShaderTexture(0, ARTIFACT_BAR_RESOURCE);
-		// i & height - 16 are equivalent to the x & y found in the old renderer
-		guiGraphics.blit(ARTIFACT_BAR_RESOURCE, x, y, 0,
-				0, 62, 22, 62, 22);
-		int i = 0;
-		for (KeyMapping key : List.of(CuriosKeyBindings.activateArtifact1, CuriosKeyBindings.activateArtifact2,
-				CuriosKeyBindings.activateArtifact3)) {
-			String keyName = key.getKey().getDisplayName().getString();
-			guiGraphics.drawString(getFont(), keyName, x + 19 + (i * 20) - getFont().width(keyName), y + 3,
-					0xFFFFFF, true);
-			i++;
+					RenderSystem.setShaderTexture(0, ARTIFACT_BAR_RESOURCE);
+					// i & height - 16 are equivalent to the x & y found in the old renderer
+					guiGraphics.blit(ARTIFACT_BAR_RESOURCE, x, y, 0,
+							0, 62, 22, 62, 22);
+					int i = 0;
+					for (KeyMapping key : List.of(CuriosKeyBindings.activateArtifact1,
+							CuriosKeyBindings.activateArtifact2,
+							CuriosKeyBindings.activateArtifact3)) {
+						ItemStack artifact = stacks.getStackInSlot(i);
+						if (!artifact.isEmpty() && artifact.getItem() instanceof ArtifactItem)
+							guiGraphics.renderItem(artifact, x + (i * 20) + 3, y + 3);
+						String keyName = key.getKey().getDisplayName().getString();
+						guiGraphics.drawString(getFont(), keyName,
+								x + 19 + (i * 20) - getFont().width(keyName),
+								y + 3,
+								0xFFFFFF, true);
+						i++;
+					}
+
+				}
+			});
 		}
 	}
 }
